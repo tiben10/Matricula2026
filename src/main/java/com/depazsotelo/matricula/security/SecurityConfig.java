209129df05d95ec2e86eea5470742d4e47daaf96
Gato <royconcepcion10@gmail.com>
2026-07-04 20:59:30 -0500

    Agregar anotación a la clase para habilitar @PreAuthorize

--- src/main/java/com/depazsotelo/matricula/security/SecurityConfig.java
+++ src/main/java/com/depazsotelo/matricula/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +18,7 @@ import org.springframework.web.cors.CorsConfigurationSource; // IMPORTANTE
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource; // IMPORTANTE
 import java.util.Arrays; // IMPORTANTE
 
+@EnableMethodSecurity // MEJORA: habilita @PreAuthorize a nivel de metodo
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
